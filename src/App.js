1e715627ba60ab98465502f3425a5fe2d3153083
Suraj <surajrajgp@gmail.com>
2023-12-30 03:25:11 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -246,7 +246,7 @@ function MovieDetails({ selectedId, OnCloseMovie,onAddWatched,watched }) {
     async function getMovieDetails() {
       setIsLoading(true);
       const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
       );
       const data = await res.json();
       setMovie(data);
