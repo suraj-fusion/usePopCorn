608209b09839957cf20460a42b4bdff746b98e08
Suraj <surajrajgp@gmail.com>
2023-12-30 03:22:58 +0530

    bug fix

--- src/useMovies.js
+++ src/useMovies.js
@@ -15,7 +15,7 @@ export function useMovies(query){
               setIsLoading(true);
     
               const res = await fetch(
-                `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,{signal:controller.signal}
+                `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,{signal:controller.signal}
     
               );
     
